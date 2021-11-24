c303672dd99b0304d9d3fabef4923ef324501139
fritzkuechli <fritzchensrevenge@gmail.com>
2021-11-24 11:24:55 +0100

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,10 @@ public class Main {
     public static void main(String[] args) {
         int size = 100;
         ArrayList<Integer> listTwo = new ArrayList<>();
-        int[] list = {15, 9, 8, 1, 4, 11, 7, 12, 13, 6, 5, 3, 16, 2, 10, 14};
+        Integer [] list = new Integer[]{15, 9, 8, 1, 4, 11, 7, 12, 13, 6, 5, 3, 16, 2, 10, 14};
+        int sizeList = list.length;
         int value;
+        boolean swapped;
         Random rand = new Random();
         Set<Integer> set = new HashSet<Integer>();
 
@@ -18,12 +20,27 @@ public class Main {
         }
 
         listTwo.addAll(set);
-
+        /*
         for (int i : listTwo) {
             System.out.println(i);
         }
+         */
 
-        //Bubble Sort
+        // Bubble Sort
+
+        do {
+            swapped = false;
+            int i = 0;
+
+            while (i < list.length - 1  ){
+                if (list[i] > list[i+1]){
+                Collections.swap(Arrays.asList(list), i, i + 1 );
+                swapped = true;
+                }
+                i++;
+            }
+            sizeList--;
+        } while (swapped);
 
 
         for (int i : list){
