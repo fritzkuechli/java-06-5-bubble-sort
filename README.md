e740e83cfbc02a51ab73b11da16497bb11167670
fritzkuechli <fritzchensrevenge@gmail.com>
2021-11-24 10:57:54 +0100

    pseudo code changed image

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Implementiere einen Bubble Sort Algorithmus!
 
 Eine super Visualisierung zu Sortieralgorithmen findest du auf http://sorting.at/. 
 Eine gute Erklärung zur Vorgehensweise des Bubble sort findest du auf Wikipedia: https://de.wikipedia.org/wiki/Bubblesort
+![Bubble-Sort](bubble_sort.png)
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
+import java.util.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int size = 100;
+        ArrayList<Integer> listTwo = new ArrayList<>();
+        int[] list = {15, 9, 8, 1, 4, 11, 7, 12, 13, 6, 5, 3, 16, 2, 10, 14};
+        int value;
+        Random rand = new Random();
+        Set<Integer> set = new HashSet<Integer>();
+
+        // hm not randomized
+        while (set.size() != size) {
+            set.add(rand.nextInt(size + 1));
+        }
+
+        listTwo.addAll(set);
+
+        for (int i : listTwo) {
+            System.out.println(i);
+        }
+
+        //Bubble Sort
+
+
+        for (int i : list){
+            System.out.println(i);
+        }
     }
 }
